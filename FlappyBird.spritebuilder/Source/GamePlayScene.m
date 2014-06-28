0d98f81820dbb6f732753162060ead0e30cc5a36
Philip James <pjj@philipjohnjames.com>
2014-06-28 11:53:20 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,8 @@
 - (void)initialize
 {
     // your code here
+    character = (Character *)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +16,8 @@
     // put update code here
 }
 
-// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [character flap];
+}
 
 @end
